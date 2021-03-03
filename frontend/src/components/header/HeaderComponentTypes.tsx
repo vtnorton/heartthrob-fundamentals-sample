1a650e476cb4188ee108d5ecda916a1fa9a4f993
Vítor Norton <vtnorton@outlook.com>
2021-03-03 20:15:41 -0300

    moved components to heartthrob-react

--- frontend/src/components/header/HeaderComponentTypes.tsx
+++
@@ -1,8 +0,0 @@
-import { IIconProps } from '@fluentui/react'
-
-export interface MenuItemType {
-	icon: IIconProps
-	name: string
-	location?: string
-	action?: any // TODO: Descobrir
-}
--- frontend/src/features/index.tsx
+++ frontend/src/features/index.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { ConnectedRouter } from 'connected-react-router'
 import { Redirect, Route, Switch } from 'react-router-dom'
 import { initializeIcons } from '@fluentui/react'
+import { MenuItemType, HeaderComponent } from 'heartthrob-react'
 
 import { history } from '../init-redux'
 import AccountRoutes from './account/AccountRoutes'
 import DashboardRoutes from './dashboard/DashboardRoutes'
-import HeaderComponent from '../components/header/HeaderComponent'
 import './index.scss'
 import SystemRoutes from './system/SystemRoutes'
 
@@ -17,6 +17,19 @@ const privateRoutes = [].concat(SystemRoutes(), DashboardRoutes())
 export default () => {
 	initializeIcons()
 	const isAuthenticated = () => token !== null
+
+	const accountMenu: MenuItemType[] = [
+		{ icon: { iconName: 'UserOptional' }, name: 'Gerenciar conta', location: '/manage/profile' },
+		// { icon: manageProfileIcon, name: 'Enviar feedbacks', location: '/manage/feedback/send' },
+		//	{ icon: manageProfileIcon, name: 'Feedbacks', location: '/manage/feedback' },
+		{ icon: { iconName: 'SignOut' }, name: 'Sair', action: signOut },
+	]
+
+	function signOut() {
+		localStorage.removeItem('token')
+		window.location.href = '/account/login'
+	}
+
 	const PrivateRoute = ({ component: Component, ...rest }) => (
 		<Route
 			{...rest}
@@ -24,7 +37,7 @@ export default () => {
 				if (isAuthenticated())
 					return (
 						<>
-							<HeaderComponent token={token} />
+							<HeaderComponent token={token} accountMenu={accountMenu} />
 							<div className='content'>
 								<Component {...props} />
 							</div>
