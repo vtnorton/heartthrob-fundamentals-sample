fbff4198fbcbd57d52a5503a114c0a64b5bb3677
Vítor Norton <vtnorton@outlook.com>
2021-03-21 13:10:59 -0300

    Disparando o dispatch

--- frontend/src/features/system/profile/Panels/EditProfilePanel.tsx
+++ frontend/src/features/system/profile/Panels/EditProfilePanel.tsx
@@ -1,7 +1,9 @@
 import { MouseEventHandler, useState } from 'react'
 import { TextField, DatePicker, PrimaryButton, IIconProps, Panel, PanelType } from '@fluentui/react'
 import Taskbar from 'heartthrob-react/src/components/Card/Taskbar/Taskbar'
-import { ProfileInfo } from '../ProfileTypes'
+import { EditProfileRequest, ProfileInfo } from '../ProfileTypes'
+import { actions } from '../ProfileState'
+import { useDispatch } from 'react-redux'
 
 interface PropsEditProfilePanel {
 	isOpenedPanel: boolean,
@@ -9,16 +11,30 @@ interface PropsEditProfilePanel {
 	profile: ProfileInfo,
 }
 
-
 const EditProfilePanel = (props: PropsEditProfilePanel) => {
 	const [firstName, setFirstName] = useState(props.profile.firstName)
 	const [lastName, setLastName] = useState(props.profile.lastName)
+	const [jobTitle, setJobTitle] = useState(props.profile.jobTitle)
+	const [birthday, setBirthday] = useState(props.profile.birthday)
+	
+	const dispatch = useDispatch()
+	function editProfile(){
+		const newProfile: EditProfileRequest = {
+			firstName: firstName,
+			lastName: lastName,
+			jobTitle: jobTitle,
+			birthday: birthday,
+			userName: props.profile.userName,
+		}
+
+		dispatch(actions.postProfileInfo(newProfile))
+	}
 
 	const panelTaskbar = () => {
 
 		const editProfilePanel = () => {
 			const profileEditIcon: IIconProps = { iconName: 'Save' }
-			return <PrimaryButton text='Salvar alterações' iconProps={profileEditIcon} onClick={props.onDismiss} />
+			return <PrimaryButton text='Salvar alterações' iconProps={profileEditIcon} onClick={editProfile} />
 		}
 
 		return <Taskbar buttons={editProfilePanel()} />
@@ -29,25 +45,28 @@ const EditProfilePanel = (props: PropsEditProfilePanel) => {
 			<>
 				<div className='row'>
 					<div className='col-md-6 col-sm-6'>
-						<TextField name='firstName' value={props.profile.firstName} label='Nome' placeholder='João' onChange={(e) => setFirstName((e.target as HTMLTextAreaElement).value)} />
+						<TextField name='firstName' required defaultValue={props.profile.firstName} label='Nome' placeholder='João' onChange={(e) => setFirstName((e.target as HTMLTextAreaElement).value)} />
 					</div>
 					<div className='col-md-6 col-sm-6'>
-						<TextField name='lastName' value={props.profile.lastName} label='Sobrenome' placeholder='Silva' onChange={(e) => setLastName((e.target as HTMLTextAreaElement).value)} />
+						<TextField name='lastName' required defaultValue={props.profile.lastName} label='Sobrenome' placeholder='Silva' onChange={(e) => setLastName((e.target as HTMLTextAreaElement).value)} />
 					</div>
 				</div>
 				<div className='row'>
 					<div className='col-md-6 col-sm-6'>
 						<TextField
 							name='firstName'
-							value={props.profile.jobTitle}
+							defaultValue={props.profile.jobTitle}
 							label='Profissão'
 							placeholder='Advogado'
-							onChange={(e) => setFirstName((e.target as HTMLTextAreaElement).value)}/>
+							onChange={(e) => setJobTitle((e.target as HTMLTextAreaElement).value)}/>
 					</div>
 				</div>
 				<div className='row'>
 					<div className='col-md-6 col-sm-6'>
-						<DatePicker value={props.profile.birthday} label='Data de nascimento' placeholder='30/03/2000' />
+						<DatePicker value={props.profile.birthday} 
+							label='Data de nascimento' 
+							placeholder='30/03/2000' 
+							onSelectDate={(date) => setBirthday(date)} />
 					</div>
 				</div>
 			</>
