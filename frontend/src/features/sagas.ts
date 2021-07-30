af078394733cec0a061e85f849da2f05cc4d7254
vítor norton <vtnorton@outlook.com>
2021-07-30 17:55:02 -0300

    This PR fixes build been broken because of breaking changes on the heartthrob-fundamentals-core  (#17)
    * updated to fix build breaking
    
    * Updated to fix breaking changes

--- frontend/src/features/sagas.ts
+++ frontend/src/features/sagas.ts
@@ -2,5 +2,8 @@ import { all, AllEffect, ForkEffect } from 'redux-saga/effects'
 import HeartthrobSagas from 'heartthrob-fundamentals/core/features/sagas'
 
 export default function* (): Generator<AllEffect<Generator<ForkEffect<never>, void, unknown>>, void, unknown> {
-	yield all([...HeartthrobSagas])
+	yield all([
+		HeartthrobSagas.AccountSaga(),
+		HeartthrobSagas.ProfileSaga(),
+	])
 }
